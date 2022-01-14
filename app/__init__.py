2f7d7add2a745c14c1725a322e4c87118391f7d1
Steve Njuguna <stevenjuguna93@gmail.com>
2022-01-14 13:11:07 +0300

    Styled register page

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from dotenv import load_dotenv
 
 # Initializing application
 app = Flask(__name__)
 
 from app import views
+
+load_dotenv('.env')
+app.config.from_pyfile('config.py')
