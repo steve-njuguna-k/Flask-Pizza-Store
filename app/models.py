06f79d9a647ad34adb3aeec1bb8b7a8740dce040
Steve Njuguna <stevenjuguna93@gmail.com>
2022-01-14 12:29:28 +0300

    Added regiter form to HTML page

---
+++ app/models.py
@@ -0,0 +1,14 @@
+
+# User
+# ----
+# - Username
+# - Email
+# - Password
+
+# Pizza
+# -----
+# - Name
+# - Prize
+# - Toppings
+# - Size
+# - Quantity
--- app/views.py
+++ app/views.py
@@ -1,6 +1,16 @@
 from app import app
 from flask import render_template
+from .forms import RegisterForm
 
 @app.route('/')
 def home():
-    return render_template('Base.html')
+    return render_template('Index.html')
+
+@app.route('/register/')
+def register():
+    form = RegisterForm()
+    return render_template('Register.html', form = form)
+
+@app.route('/login/')
+def login():
+    return render_template('Login.html')
